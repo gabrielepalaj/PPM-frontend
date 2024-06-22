d119eaff17e120ab52edb764b3ecd13a9227ba09
gabriele.palaj <gabriele.palaj@logaut.it>
2024-06-22 19:34:51 +0200

    Create chiamate API backend

---
+++ src/services/api.js
@@ -0,0 +1,32 @@
+// src/services/api.js
+
+import axios from 'axios';
+import { getToken } from '../utils/auth';
+
+const API_URL = 'http://localhost:5000';
+
+const apiClient = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+apiClient.interceptors.request.use((config) => {
+    const token = getToken();
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export const loginUser = (username, password) => {
+    return apiClient.post('/login', { username, password });
+};
+
+export const registerUser = (username, password, email) => {
+    return apiClient.post('/register', { username, password, email });
+};
+
