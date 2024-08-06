6dc42e7b0cbd8c534c72c078802cb6474406948e
gabriele.palaj <gabriele.palaj@logaut.it>
2024-08-06 22:12:40 +0200

    Migliorata visualizzazione changes

--- src/components/ChangeDetailsModal.jsx
+++ src/components/ChangeDetailsModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import '../styles/ChangeDetailsModal.css';
 
 const ChangeDetailsModal = ({ website, show, onHide, onMarkAsRead }) => {
     const handleMarkAsRead = () => {
@@ -11,28 +12,49 @@ const ChangeDetailsModal = ({ website, show, onHide, onMarkAsRead }) => {
 
     return (
         <Modal show={show} onHide={onHide} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-            <Modal.Header>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Change Details for {website.name}
+                </Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <h4>URL</h4>
                 <p>{website.url}</p>
-                {website.last_change && (
-                    <>
-                        <h4>Last Change</h4>
-                        <img src={`data:image/png;base64,${website.last_change.screenshot}`} alt="Screenshot" style={{ width: '300px' }} />
-                        {website.previous_change && (
-                            <>
-                                <h4>Previous Change</h4>
-                                <img src={`data:image/png;base64,${website.previous_change.screenshot}`} alt="Screenshot" style={{ width: '300px' }} />
-                                <h4>Differences</h4>
-                                <img src={`data:image/png;base64,${website.differences.diff_image}`} alt="Differences" style={{ width: '300px' }} />
-                            </>
-                        )}
-                    </>
-                )}
+                <div className="changes-container">
+                    {website.last_change && (
+                        <div className="change-item">
+                            <h4>Last Change</h4>
+                            <p>Detected at: {new Date(website.last_change.change_detected_at).toLocaleString()}</p>
+                            <img
+                                src={`data:image/png;base64,${website.last_change.screenshot}`}
+                                alt="Last Change Screenshot"
+                                className="change-screenshot"
+                            />
+                            {website.last_change.change_summary && (
+                                <>
+                                    <h5>Change Summary</h5>
+                                    <p>{website.last_change.change_summary}</p>
+                                </>
+                            )}
+                        </div>
+                    )}
+                    {website.previous_change && (
+                        <div className="change-item">
+                            <h4>Previous Change</h4>
+                            <p>Detected at: {new Date(website.previous_change.change_detected_at).toLocaleString()}</p>
+                            <img
+                                src={`data:image/png;base64,${website.previous_change.screenshot}`}
+                                alt="Previous Change Screenshot"
+                                className="change-screenshot"
+                            />
+                        </div>
+                    )}
+                </div>
             </Modal.Body>
             <Modal.Footer>
-                <Button onClick={handleMarkAsRead}>Mark as Read</Button>
+                {website.last_change && !website.last_change.reviewed && (
+                    <Button onClick={handleMarkAsRead}>Mark as Read</Button>
+                )}
                 <Button onClick={onHide}>Close</Button>
             </Modal.Footer>
         </Modal>
@@ -41,11 +63,23 @@ const ChangeDetailsModal = ({ website, show, onHide, onMarkAsRead }) => {
 
 ChangeDetailsModal.propTypes = {
     website: PropTypes.shape({
+        id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
         url: PropTypes.string.isRequired,
-        last_change: PropTypes.object,
-        previous_change: PropTypes.object,
-        differences: PropTypes.object,
+        last_change: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            change_detected_at: PropTypes.string.isRequired,
+            change_summary: PropTypes.string,
+            screenshot: PropTypes.string.isRequired,
+            reviewed: PropTypes.bool.isRequired,
+        }),
+        previous_change: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            change_detected_at: PropTypes.string.isRequired,
+            change_summary: PropTypes.string,
+            screenshot: PropTypes.string.isRequired,
+            reviewed: PropTypes.bool.isRequired,
+        }),
     }).isRequired,
     show: PropTypes.bool.isRequired,
     onHide: PropTypes.func.isRequired,
